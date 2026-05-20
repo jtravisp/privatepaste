b07a94193bb356e7e76f4c636882ddb5287c13f5
jtravisp <travis@travispollard.com>
2026-05-19 21:39:09 -0500

    aws and dynamo client config

--- app/cmd/server/main.go
+++ app/cmd/server/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/joho/godotenv"
-	"github.com/jtravisp/privatepaste/internal/config"
+	appconfig "github.com/jtravisp/privatepaste/internal/config"
+	"github.com/jtravisp/privatepaste/internal/store"
 )
 
 
@@ -16,12 +20,22 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     godotenv.Load()
-	cfg := config.Load()
+	cfg := appconfig.Load()
 	
+	awsCfg, err := config.LoadDefaultConfig(context.TODO(),
+    config.WithRegion(cfg.Region),
+	)
+	dynamoClient := dynamodb.NewFromConfig(awsCfg)
+	store := store.NewDynamo(dynamoClient, cfg.TableName)
+	_ = store // temporary to avoid unused variable error	
+
+	if err != nil {
+		log.Fatalf("failed to load AWS config: %v", err)
+	}
+
 	mux := http.NewServeMux()
     mux.HandleFunc("GET /health", healthCheck)
 
     log.Println("starting server on :" + cfg.Port)
     log.Fatal(http.ListenAndServe(":" + cfg.Port, mux))
-
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,5 @@
-
+ variable "aws_region" {
+    type    = string
+    default = "us-east-1"
+ }
+ 
