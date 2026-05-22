cf11c041ca989cb34b55a4792c322373734e37d7
Travis Pollard <tpollard@Traviss-MacBook-Air.local>
2026-05-22 11:09:11 -0500

    start coding handler

--- app/internal/handler/paste.go
+++ app/internal/handler/paste.go
@@ -1,1 +1,23 @@
 package handler
+
+import (
+	"net/http"
+	"github.com/jtravisp/privatepaste/internal/store"
+)
+
+type PasteHandler struct {
+    store store.Store
+}
+
+func (h *PasteHandler) CreatePaste(w http.ResponseWriter, r *http.Request) {
+	http.MethodPost(url string, contentType string, body io.Reader)
+}
+
+func (h *PasteHandler) GetPaste(w http.ResponseWriter, r *http.Request) {
+	http.MethodGet(url string, contentType string, body io.Reader)
+
+}
+
+func (h *PasteHandler) DeletePaste(w http.ResponseWriter, r *http.Request) {
+	http.MethodDelete(url string, contentType string, body io.Reader)
+}
