5e9bde490a920601a92390c41b52f4dbc979190d
jtravisp <travis@travispollard.com>
2026-05-23 17:39:27 -0500

    add handler structs

--- app/internal/handler/paste.go
+++ app/internal/handler/paste.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+
 	"github.com/jtravisp/privatepaste/internal/store"
 )
 
@@ -9,15 +10,26 @@ type PasteHandler struct {
     store store.Store
 }
 
+type CreatePasteRequest struct {
+    Ciphertext   string `json:"ciphertext"`
+    IV           string `json:"iv"`
+    BurnAfterRead bool  `json:"burn_after_read"`
+    Expiry       string `json:"expiry"`
+}
+
+type CreatePasteResponse struct {
+    ID         string `json:"id"`
+    OwnerToken string `json:"owner_token"`
+}
+
 func (h *PasteHandler) CreatePaste(w http.ResponseWriter, r *http.Request) {
-	http.MethodPost(url string, contentType string, body io.Reader)
+    // implement
 }
 
 func (h *PasteHandler) GetPaste(w http.ResponseWriter, r *http.Request) {
-	http.MethodGet(url string, contentType string, body io.Reader)
-
+    // implement
 }
 
 func (h *PasteHandler) DeletePaste(w http.ResponseWriter, r *http.Request) {
-	http.MethodDelete(url string, contentType string, body io.Reader)
+    // implement
 }
